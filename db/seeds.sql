0a98a0f68a93e5e75169986fcec7b1fc8904a49b
Baxter Cox <baxtercoxrtr@gmail.com>
2020-11-18 14:40:09 -0500

    new push

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Bacon Cheese Burger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Swiss Burger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Veggie Burger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Chili Cheese Burger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Bacon Swiss Burger', false);
